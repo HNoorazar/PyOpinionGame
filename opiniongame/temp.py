ed01a9ac3615ee8b90e651fb45c66d4a397de647
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-03-30 15:24:09 -0700

    Bio-StocasticAdjacency

--- opiniongame/temp.py
+++ opiniongame/temp.py
@@ -1,76 +1,11 @@
 import numpy as np
 
-def make_adj(pop_size, topology):
-    """
-    Create an adjacency matrix for a few specific types of graph given a population
-    size and topology.  Supported types: star, full, grid.
-    """
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
-    adj = None
-
-    if topology == 'star':
-        adj = np.zeros((pop_size, pop_size))
-        adj[:, [-1]] = 1
-        adj[[-1], :] = 1
-        adj[-1, -1] = 0
-    elif topology == 'full':
-        adj = np.zeros((pop_size, pop_size))
-        adj = np.ones((pop_size, pop_size)) - np.identity(pop_size)
-    elif topology == 'grid':
-        adj = generateGridAdj(pop_size)
-    return adj
-
-#
-# generate a grid graph.  computes the dimensions for the grid by
-# calculating the prime factors (with duplication) of the population
-# size and lets the nrows be the product of the odd elements of the
-# factor list, and the ncols be the product of the even elements of
-# the factor list.
-#
-def generateGridAdj(pop_size):
-    def primes(n):
-        primfac = []
-        d = 2
-        while d*d <= n:
-            while (n % d) == 0:
-                primfac.append(d)  # supposing you want multiple factors repeated
-                n //= d
-            d += 1
-        if n > 1:
-            primfac.append(n)
-        return primfac
-
-    factors = primes(pop_size)
-    nrows = np.prod(factors[0::2])
-    ncols = np.prod(factors[1::2])
-
-    adj = np.zeros((nrows * ncols, nrows * ncols))
-
-    for i in range(nrows):
-        for j in range(ncols):
-            idx = i*ncols + j
-            if i-1 >= 0:
-                nidx = (i-1)*ncols + j
-                adj[idx, nidx] = 1
-                adj[nidx, idx] = 1
-            if i+1 < nrows:
-                nidx = (i+1)*ncols + j
-                adj[idx, nidx] = 1
-                adj[nidx, idx] = 1
-            if j-1 >= 0:
-                nidx = i*ncols + (j-1)
-                adj[idx, nidx] = 1
-                adj[nidx, idx] = 1
-            if j+1 < ncols:
-                nidx = i*ncols + (j+1)
-                adj[idx, nidx] = 1
-                adj[nidx, idx] = 1
-
-    return adj
-
-
-import numpy as np
-
+This is a temporary script file.
+"""
 # Matrices and vectors are numpy arrays!
 
 ############################################################################
@@ -121,7 +56,7 @@ def MakeUpper(n):
     upper[np.tril_indices(size, -1)] = np.arange(1, noElements+1)
     return upper        
     
-def MakeBigMatrix( n, p, ubound ):
+def MakeBigMatrix(n, p, ubound):
     # This function makes a adjacency matrix whose entries
     # will be probabilities of interactions with p communites of size n.
 
@@ -129,7 +64,7 @@ def MakeBigMatrix( n, p, ubound ):
     # but between communities are different.
     
     # This matrix will turn into a bio-stochastic later.
-    BigMatrix = np.zeros(( n*p , n*p ));
+    BigMatrix = np.zeros((n*p , n*p));
     d = (1. / (n-1)) * np.ones((n,n)) - np.diag(np.ones(n) / (n-1));
     for rowCount in range(p):
         for colCount in range(rowCount, p):
@@ -146,7 +81,7 @@ def MakeBigMatrix( n, p, ubound ):
                 # Here we take care of interactions between different
                 # communities. The counting purposes are done for
                 # lower part of bigMatrix.
-                BigMatrix[rowStart:rowEnd, colStart:colEnd] = np.random.uniform(low = 0, high = ubound, size = (n, n));
+                BigMatrix[rowStart:rowEnd, colStart:colEnd] = np.random.uniform(low = 0,high = ubound, size = (n, n));
                 BigMatrix[colStart:colEnd, rowStart:rowEnd] = BigMatrix[rowStart:rowEnd, colStart:colEnd]
     return BigMatrix
     
@@ -168,6 +103,5 @@ def CommunitiesMatrix( popSize , comNo , upperBound):
 def PickTwo(adj):
     n = np.shape(adj)[0]
     i = np.random.randint(0, n)
-    j = np.random.choice(range(n), p = adj[i,:].tolist())
+    j = np.random.choice(range(n), p = (adj[i,:]/np.norm(adj[i,:])).tolist())
     return [i,j]
-    
