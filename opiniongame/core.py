8e645a23d3c4529fb592d38dede9328917f11534
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-03-31 12:23:45 -0700

    run_all_experiments-Core

--- opiniongame/core.py
+++ opiniongame/core.py
@@ -93,6 +93,9 @@ def one_step(config, state, ufunc):
 
     return (curOpinions, total_change, pairs)
 
+#
+# This function runs one single experiment, from t=0 to t=t_conv
+#
 def run_until_convergence(config, state, ufunc):
     # initialize history with 3D array at time =0
     state.initializeHistory()
@@ -119,3 +122,23 @@ def run_until_convergence(config, state, ufunc):
     state.iterCount = iterCount
 
     return state
+#
+# This function runs a set of experiments with the same initial opinions.
+# Number of Experiments are given in config file.
+# Goal is to have different orders of interactions with the same initials.
+#
+def run_all_experiments(config, state, ufunc):
+    # initialize history with a dictionary.
+    all_experiments_history = {}
+    
+    for experiment_no in range(config.no_of_experiments):
+        # copy the instance here, for use of refer by refrence thing! 
+        # O.W. it would be gone.
+        stateCopy = copy.deepcopy(state)
+        
+        # 1- run out of all experiments:
+        history = run_until_convergence(config, state, ufunc)
+        
+        all_experiments_history[ 'experiment' + str(experiment_no + 1 )] = history
+    return all_experiments_history
+    
