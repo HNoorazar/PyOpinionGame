2b654d8a2f3b40e0260d3bdc127430458166bcbc
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-04-02 17:45:38 -0700

    communityExperimentDriver

--- DriverCommExp.py
+++ DriverCommExp.py
@@ -33,8 +33,8 @@ config.printOut()
 # seed PRNG: must do this before any random numbers are
 # ever sampled during default generation
 #
-# print("SEEDING PRNG: "+str(config.startingseed))
-# np.random.seed(config.startingseed)
+print("SEEDING PRNG: "+str(config.startingseed))
+np.random.seed(config.startingseed)
 
 state = og_state.WorldState.fromCmdlineArguments(cmdline, config)
 #
@@ -44,22 +44,46 @@ numberOfCommunities = 5
 communityPopSize    = 50
 config.popSize = numberOfCommunities * communityPopSize
 
-uppBound = .000001      # upper bound of probability interaction between communities.
-state.adj = og_adj.CommunitiesMatrix(communityPopSize , numberOfCommunities , uppBound)
 
-state.initialOpinions = og_opinions.initialize_opinions(config.popSize, config.ntopics)
-state.couplingWeights = og_coupling.weights_no_coupling(config.popSize, config.ntopics)
+# List of upper bound probability of interaction between communities
+uppBound_list = np.arange(0.000001, 0.001, 0.0001) 
+
+# Number of different initial opinions.
+noInitials = np.arange(100)
+
+# Number of different game orders.
+noGames = np.arange(100)
 
 config.learning_rate = 0.1
 tau = 0.62
-
 config.iterationMax = 20000
+
 #
 # functions for use by the simulation engine
 #
 ufuncs = og_cfg.UserFunctions(og_select.PickTwoWeighted,
                               og_stop.iterationStop,
                               og_pot.createTent(tau))
+# Run experiments with different adjacencies, different initials, and different order of games.
+for upperBound in uppBound_list:
+    # Generate different adjacency matrix with different prob. of interaction
+    # between different communities
+    state.adj = og_adj.CommunitiesMatrix(communityPopSize , numberOfCommunities , upperBound)
+
+    for countInitials in noInitials:
+        # for each adjacency, generate 100 different initial opinions
+        state.initialOpinions = og_opinions.initialize_opinions(config.popSize, config.ntopics)
+        state.couplingWeights = og_coupling.weights_no_coupling(config.popSize, config.ntopics)
+        
+        all_experiments_history = {}
+
+        for gameOrders in noGames:
+            state = og_core.run_until_convergence(config, state, ufuncs)
+            print "One Experiment Done"
+            all_experiments_history[ 'experiment' + str(gameOrders+1 )] = state.history
+        og_io.saveMatrix('output' + '-' + str(upperBound) + '-' + str(countInitials+1) + '.mat', all_experiments_history)
+
+
 
 # ToDo: short version: We should compare parameters provided by Driver.py and default.
 #       just like from command line vs. default.
@@ -71,8 +95,6 @@ ufuncs = og_cfg.UserFunctions(og_select.PickTwoWeighted,
 # we have to fix that! initialOpinion should have a proper size, based on
 # adjacency provided in the driver.
 
-state = og_core.run_until_convergence(config, state, ufuncs)
 
-rdict = {}
-rdict['history'] = state.history
-og_io.saveMatrix('output.mat', rdict)
+# ToDo : Stuff being printer on the command lines are nor correct
+# if we provide stuff in the driver. 
