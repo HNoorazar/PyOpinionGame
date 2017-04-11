550898d924f84e52c88dc567db00abf7d3ca9e4e
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-04-11 14:03:00 -0700

    variance-Cure

--- DriverCommExp.py
+++ DriverCommExp.py
@@ -58,7 +58,9 @@ noGames = np.arange(1)
 config.learning_rate = 0.1
 config.uniqStrength = .001
 tau = 0.62
-config.iterationMax = 1000
+config.iterationMax = 100
+
+config.printOut()
 
 #
 # functions for use by the simulation engine
