4dc8a4d9af8cd5dd2b5664aa7dc7d02965d5b7d3
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-05-02 11:27:12 -0700

    New Driver

--- uniqFreqDriver.py
+++ uniqFreqDriver.py
@@ -40,10 +40,26 @@ communityPopSize    = 25
 config.popSize = numberOfCommunities * communityPopSize
 
 # List of upper bound probability of interaction between communities
-uppBound_list = np.arange(0.001, 0.0161, 0.003)
-uppBound_list = np.array([0.014, 0.015])
+uppBound_list = np.array([.001, 0.004, 0.007, 0.01, 0.011, 0.012, 0.013, 0.014, 0.015, 0.016])
+
+#
 # List of uniqueness Strength parameter
-individStrength = np.arange(0, 0.1, 0.1)
+#
+a = np.arange(0.00001, 0.000251, 0.00003)
+
+# part 0
+individStrength = a[0:1]
+
+# part 1
+individStrength = a[1:4]
+
+# part 2
+individStrength = a[4:5]
+
+# part 3
+individStrength = a[5:7]
+
+#individStrength = a[7:]
 
 config.learning_rate = 0.1
 tau = 0.62
@@ -56,8 +72,8 @@ ufuncs = og_cfg.UserFunctions(og_select.PickTwoWeighted,
                               og_stop.iterationStop,
                               og_pot.createTent(tau))
                               
-noInitials = np.arange(1) # Number of different initial opinions.
-noGames = np.arange(25)    # Number of different game orders.
+noInitials = np.arange(20) # Number of different initial opinions.
+noGames = np.arange(50)    # Number of different game orders.
 # Run experiments with different adjacencies, different initials, and different order of games.
 for uniqForce in individStrength:
     config.uniqstrength = uniqForce
@@ -65,7 +81,7 @@ for uniqForce in individStrength:
         # Generate different adjacency matrix with different prob. of interaction
         # between different communities
         state.adj = og_adj.CommunitiesMatrix(communityPopSize, numberOfCommunities, upperBound)
-        print"(uniqForce, upperBound)  =(", uniqForce, "," , upperBound , ")"            
+        print"(upperBound, uniqForce) = (", upperBound, "," , uniqForce , ")"            
         for countInitials in noInitials:
             # for each adjacency, generate 100 different initial opinions
             # state.initialOpinions = og_opinions.initialize_opinions(config.popSize, config.ntopics)
@@ -84,6 +100,9 @@ for uniqForce in individStrength:
             for gameOrders in noGames:
                 #cProfile.run('og_core.run_until_convergence(config, state, ufuncs)')
                 state = og_core.run_until_convergence(config, state, ufuncs)
-                all_experiments_history[ 'experiment' + str(gameOrders+1)] = state.history[0:state.nextHistoryIndex,:,:]
+                state.history = state.history[0:state.nextHistoryIndex,:,:]
+                idx_IN_columns = [i for i in xrange(np.shape(state.history)[0]) if (i % config.popSize) == 0]
+                state.history = state.history[idx_IN_columns,:,:]
+                all_experiments_history[ 'experiment' + str(gameOrders+1)] = state.history
             og_io.saveMatrix('uB' + str(upperBound) + '*uS' + str(config.uniqstrength) + 
                              '*initCount' + str(countInitials+1) + '.mat', all_experiments_history)
