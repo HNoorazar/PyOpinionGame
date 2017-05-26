b6bb6b7ca0ad4ea6a85351f503bedfc13825773a
Hossein Noorazar <HNoorazar@users.noreply.github.com>
2017-05-26 12:41:43 -0700

    experDriver

--- experDriver.py
+++ experDriver.py
@@ -40,12 +40,12 @@ communityPopSize    = 25
 config.popSize = numberOfCommunities * communityPopSize
 
 # List of upper bound probability of interaction between communities
-uppBound_list = np.array([.0012])
+uppBound_list = np.array([.000012])
 
 #
 # List of uniqueness Strength parameter
 #
-individStrength = np.array([0.001])
+individStrength = np.array([0.0001])
 
 config.learning_rate = 0.1
 tau = 0.62
@@ -75,9 +75,9 @@ for uniqForce in individStrength:
          
             # Pick three communities with similar opinions to begin with!
             state.initialOpinions = np.zeros((config.popSize, 1))
-            state.initialOpinions[0:25]  = np.random.uniform(low=0.0, high=.18, size=(25,1))
-            state.initialOpinions[25:50] = np.random.uniform(low=0.40, high=.60, size=(25,1))
-            state.initialOpinions[50:75] = np.random.uniform(low=0.82, high= 1, size=(25,1))
+            state.initialOpinions[0:25]  = np.random.uniform(low=0.08, high=.1, size=(25,1))
+            state.initialOpinions[25:50] = np.random.uniform(low=0.49, high=.51, size=(25,1))
+            state.initialOpinions[50:75] = np.random.uniform(low=0.9, high= .92, size=(25,1))
    
             state.couplingWeights = og_coupling.weights_no_coupling(config.popSize, config.ntopics)
             all_experiments_history = {}
